002af2061dda704c9dd346b6e8fc130612c4f2d8
ichi <w.konrad60@gmail.com>
2017-11-07 18:53:30 +0100

    zadanie 14.6

--- script.js
+++ script.js
@@ -1,2 +1,81 @@
-var app = React.createElement(App);
-ReactDOM.render(app, document.getElementById('app'));
+function calc() {
+	console.log(1+1);
+}
+
+var Counter = React.createClass({
+
+	getInitialState: function() {
+		return {
+			counter: 0
+		};
+	},
+	componentWillMount() {
+		return (
+			calc(),
+			console.log(this.props.mleko)
+		);
+	},
+	getDefaultProps() {
+		mleko: 'jajko'
+	},
+
+	increment: function() {
+		this.setState({
+			counter: this.state.counter + 1
+		});
+	},
+
+	decrement: function() {
+		this.setState({
+			counter: this.state.counter - 1
+		});
+	},
+
+	render: function() {
+		return React.createElement('div', {className: 'div1'},
+			React.createElement('span', {}, 'Licznik ' + this.state.counter),
+			React.createElement('button', {onMouseEnter: this.increment}, 'Zwiększanie'),
+			React.createElement('button', {onMouseLeave: this.decrement}, 'Zmniejszanie')
+		);
+	}
+});
+
+var element = React.createElement(Counter);
+ReactDOM.render(element, document.getElementById('app'));
+
+var Counter2 = React.createClass({
+	getInitialState: function() {
+		return {
+			counter: 0
+		};
+	},
+
+	increment: function() {
+		this.setState({
+			counter: this.state.counter + 1
+		});
+	},
+
+	decrement: function() {
+		this.setState({
+			counter: this.state.counter - 1
+		});
+	},
+
+	componentWillUnmount: function() {
+		console.log('mleko');
+	},
+
+	render: function() {
+		return React.createElement('div', {className: 'div2'},
+			React.createElement('span', {}, 'Licznik ' + this.state.counter),
+			React.createElement('button', {onClick: this.increment}, 'Zwiększanie'),
+			React.createElement('button', {onClick: this.decrement}, 'Zmniejszanie')
+		);
+	}
+});
+
+
+var element2 = React.createElement(Counter2);
+
+ReactDOM.render(element2, document.getElementById('app2'));
